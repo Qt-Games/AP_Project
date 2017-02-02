6c21c7845679963ea658d46c83e40b98a0536072
Farid Zandi <faridzs76@gmail.com>
2017-02-02 08:36:41 +0330

    Created DestructibleObject

---
+++ DestructableObject.h
@@ -0,0 +1,16 @@
+//
+// Created by Y50 on 2/2/2017.
+//
+
+#ifndef AP_PROJECT_DESTRUCTABLEOBJECT_H
+#define AP_PROJECT_DESTRUCTABLEOBJECT_H
+
+#include <scrollingobject.h>
+
+class DestructableObject : public ScrollingObject{
+
+
+};
+
+
+#endif //AP_PROJECT_DESTRUCTABLEOBJECT_H

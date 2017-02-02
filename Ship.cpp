0dff43b653fc493833ec59fd180bc556a00d6c8e
omid <onajafi141@gmail.com>
2017-02-02 16:45:27 +0330

    Added ship pixmaps

--- Ship.cpp
+++ Ship.cpp
@@ -3,6 +3,23 @@
 //
 
 #include "Ship.h"
+#include "GraphicScene.h"
 
 Ship::Ship(int posX, int posY, Direction direction) :
-        NonPassingThroughGrassObject(posX, posY, Ship::speed, direction) { }
+        NonPassingThroughGrassObject(posX, posY, Ship::speed, direction) {
+
+    QGraphicsPixmapItem* shippxmap=new QGraphicsPixmapItem();
+    QString tmpstr(RES_PATH);
+    tmpstr.append("/Ship.png");
+
+    QImage image(tmpstr);
+    image=image.scaled(sizeX,sizeY);
+
+    shippxmap->setPixmap(QPixmap::fromImage(image));
+    setGraphicObject(shippxmap);
+
+    shippxmap->setPos(posX,posY);
+
+    GraphicScene::getInstance()->addItem(shippxmap);
+
+}

6773e020f0a82273d4905729f5ced698bd9d759a
omid <onajafi141@gmail.com>
2017-02-02 08:53:31 +0330

    - added EnemyObjectsClass

---
+++ EnemyObject.h
@@ -0,0 +1,21 @@
+//
+// Created by dot_blue on 02/02/17.
+//
+
+#ifndef AP_PROJECT_ENEMYOBJECT_H
+#define AP_PROJECT_ENEMYOBJECT_H
+
+#include "DestructableObject.h"
+
+class EnemyObject:public DestructableObject {
+public:
+    EnemyObject(int posX, int posY, int speed, Direction direction);
+
+    void move();
+    void canPassThroughMapObjects();
+
+
+};
+
+
+#endif //AP_PROJECT_ENEMYOBJECT_H

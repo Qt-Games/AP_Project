2fd59b73e81cbe4d035ba61e92f0e746a0720de2
Farid Zandi <faridzs76@gmail.com>
2017-02-02 08:40:36 +0330

    Completing Object Class

--- DestructableObject.h
+++ DestructableObject.h
@@ -5,11 +5,19 @@
 #ifndef AP_PROJECT_DESTRUCTABLEOBJECT_H
 #define AP_PROJECT_DESTRUCTABLEOBJECT_H
 
-#include <scrollingobject.h>
+
+#include "ScrollingObject.h"
 
 class DestructableObject : public ScrollingObject{
 
 
+public:
+    DestructableObject(int posX, int posY, int speed, Direction direction) : ScrollingObject(posX, posY, speed,
+                                                                                             direction) { }
+
+    void Destruct(){
+
+    }
 };
 
 

68ec9b1b0f3c117b38f455a47e986d1b24ee9912
omid <onajafi141@gmail.com>
2017-02-02 08:22:50 +0330

    - added ScrollingObject Class

---
+++ ScrollingObject.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dot_blue on 02/02/17.
+//
+
+#include "ScrollingObject.h"
---
+++ ScrollingObject.h
@@ -0,0 +1,14 @@
+//
+// Created by dot_blue on 02/02/17.
+//
+
+#ifndef AP_PROJECT_SCROLLINGOBJECT_H
+#define AP_PROJECT_SCROLLINGOBJECT_H
+
+
+class ScrollingObject {
+
+};
+
+
+#endif //AP_PROJECT_SCROLLINGOBJECT_H

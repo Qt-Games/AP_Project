3d1d3702d1626817164432089f675aab77a3d1f3
omid <onajafi141@gmail.com>
2017-02-02 09:28:58 +0330

    - Added Jet Class

---
+++ House.cpp
@@ -0,0 +1,8 @@
+//
+// Created by dot_blue on 02/02/17.
+//
+
+#include "House.h"
+House(int posX, int posY, int speed, Direction direction){
+
+}
---
+++ House.h
@@ -0,0 +1,18 @@
+//
+// Created by dot_blue on 02/02/17.
+//
+
+#ifndef AP_PROJECT_HOUSE_H
+#define AP_PROJECT_HOUSE_H
+
+#include "ScrollingObject.h"
+
+class House:public ScrollingObject{
+public:
+    House(int posX, int posY, int speed, Direction direction);
+
+
+};
+
+
+#endif //AP_PROJECT_HOUSE_H

67cf2045c3842843d9e06023bc21b34c6edac635
omid <onajafi141@gmail.com>
2017-02-02 17:15:03 +0330

    implemented Grass Class

--- Grass.cpp
+++ Grass.cpp
@@ -3,7 +3,15 @@
 //
 
 #include "Grass.h"
+#include "GraphicScene.h"
 
 Grass::Grass(int posX, int posY, int sizeX, int sizeY, Direction direction) : ScrollingObject(posX, posY, 0, direction) {
+
+    GreenRec=new QGraphicsRectItem(posX,posY,sizeX,sizeY);
+    GreenRec->setBrush(QBrush(QColor(0,255,0,255)));
+    GreenRec->setPen(QPen(QColor(0,255,0,255)));
+
+    GraphicScene::getInstance()->addItem(GreenRec);
+
 
 }
--- Grass.h
+++ Grass.h
@@ -6,12 +6,15 @@
 #define AP_PROJECT_GRASS_H
 
 #include "ScrollingObject.h"
+#include <QBrush>
+#include <QPen>
 
 class Grass: public ScrollingObject {
 public:
 
     Grass(int posX, int posY, int sizeX, int sizeY, Direction direction);
-
+private:
+    QGraphicsRectItem* GreenRec;
 
 };
 

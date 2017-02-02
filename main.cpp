a2e6ab7dfc847cb19e01aecd78f962944a810409
omid <onajafi141@gmail.com>
2017-02-02 10:48:22 +0330

    Added Map Class & fixed threads

--- main.cpp
+++ main.cpp
@@ -2,11 +2,25 @@
 #include <QApplication>
 
 #include "Jet.h"
+#include <thread>
+#include <iostream>
+
+void func1()
+{
+    for(int i=0;i<10;i++)
+    {
+        std::cout<<"thread is being runed!!!"<<std::endl;
+    }
+
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    std::thread omid(func1);
+    omid.join();
     return a.exec();
 }

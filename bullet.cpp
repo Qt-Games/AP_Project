29b81b8e1d87c468666070636403ca0c05835e02
omid <onajafi141@gmail.com>
2017-02-05 15:07:33 +0330

    Implemented bullet Class

--- bullet.cpp
+++ bullet.cpp
@@ -3,9 +3,22 @@
 //
 
 #include "bullet.h"
+#include "GraphicScene.h"
 
 bullet::bullet(int posX, int posY, int speed, Direction direction) : Object(posX, posY, speed, direction) {
 
+    QGraphicsPixmapItem* bulletpxmap=new QGraphicsPixmapItem();
+    QString tmpstr(RES_PATH);
+    tmpstr.append("/bullet.png");
 
+    QImage image(tmpstr);
+    image=image.scaled(sizeX,sizeY);
+
+    bulletpxmap->setPixmap(QPixmap::fromImage(image));
+    setGraphicObject(bulletpxmap);
+
+    bulletpxmap->setPos(posX,posY);
+
+    GraphicScene::getInstance()->addItem(bulletpxmap);
 
 }
